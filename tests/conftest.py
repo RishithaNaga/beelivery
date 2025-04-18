f468eccd2775c6cc73ce5893f42ed7e7896eabe4
rishithaagan070890 <rishithaagan070890@gmail.com>
2025-04-18 23:43:04 +0530

    modified conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from xml.dom.xmlbuilder import Options
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture()
@@ -11,7 +11,9 @@ def setup_and_teardown(request):
     #chrome_options=Options()
     #chrome_options.add_argument()
     #driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",options=chrome_options)
-    driver=webdriver.Chrome()
+
+    service = Service(executable_path="C:\Proveway\pythonProject3\driver\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+    driver=webdriver.Chrome(service=service)
     driver.get("https://www.beelivery.com/#")
     driver.maximize_window()
     request.cls.driver=driver
